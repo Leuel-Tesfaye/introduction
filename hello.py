639e61bf31dc39384f008eef520d726ca612e215
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-11-27 14:44:17 +0300

    Python tutorial

---
+++ hello.py
@@ -0,0 +1,4 @@
+print('hello world')
+print('testing python')
+print('hello from the outside')
+# ! this is important 
---
+++ name.py
@@ -0,0 +1,2 @@
+name = input ("Name: ") # this helps to accept an input from the user in python
+print("Hello," + name) # i have declared a variable named 'name' which accepts input from the user
