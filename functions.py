1f9df7e291343e77493a653df19e66a9d841fecb
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-11-28 10:58:51 +0300

    functions and importing functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+#writing a function which takes a number and returns it's square
+
+def square (x) :
+    return x * x 
+
+
+
+def add(x,y):
+    return x + y
+print(f"The sum of the two numbers : {add(5,5)}")
