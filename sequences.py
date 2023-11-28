55310706edc26bed7cfde72e5e6491dcbcf2d1bd
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-11-28 09:02:33 +0300

    sequences generally explained

---
+++ sequences.py
@@ -0,0 +1,12 @@
+name = "harry"
+
+print(name[0])
+
+names = ("prince" , "Hope", "Power")
+print (names[1])
+
+# ? This are different types of datastructures : 
+# ? list - sequence of mutable values : means we can change the values in the list : can add and remove item to and from the list
+# tuple - sequence of immutable values: is the antonym of list : can't add and remove item to or from the list
+# set - collection of unique value
+#? dict-dictionary : collection of key value pair : in the real world the key is the 'word' we want to look up and the value is it's definition ---- any time we want to map something to some other value we can very easily look up for that value inside of this datastructures 
