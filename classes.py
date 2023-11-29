b68481f70a383e7074708aa1287ae43ec795336d
Leuel-Tesfaye <131950742+Leuel-Tesfaye@users.noreply.github.com>
2023-11-29 08:56:20 +0300

    change in classes committed

--- classes.py
+++ classes.py
@@ -7,5 +7,36 @@ class Point():
         self.x = input1
         self.y = input2
 p = Point(2,8)
+print(p.x)
+print(p.y)
+
+#? writing a program for an airline needs to keep track of booking passengers on a flight and making sure that no flight overbooked we don't want more passengers more than capacity
+
+class Flight() :
+    def __init__(self, capacity): # every flight needs some sort of capacity to know how many people can fit
+        self.capacity = capacity
+        self.passengers = []
+
+    def add_passenger (self, name):
+        if not self.open_seats (): 
+            return False    
+        self.passengers.append(name)
+        return True
+    
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+flight = Flight (3)
+
+people = ["harry", "Ron", "Prince" , "Ginny"]
+for person in people :
+    success = flight.add_passenger(person)
+    if success:
+        print(f"Added {person} to flight successfully.")
+    else:
+        print(f"No available seats for {person}")
+
+
+
 
         
